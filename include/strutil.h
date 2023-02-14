4d24bdbe04623139d5cf00c637bf10dfc290cd70
yyhome-tromb <107352731+yyhome-tromb@users.noreply.github.com>
2023-02-14 09:40:25 +0900

    make wcsStartsWith & wcsEndsWith in strutil.c

--- include/strutil.h
+++ include/strutil.h
@@ -1,1 +1,8 @@
+#pragma once
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool wcsStartsWith(wchar_t *target,size_t targetSize,wchar_t *query,size_t querySize);
+bool wcsEndsWith(wchar_t *tatget,size_t targetSize,wchar_t *query,size_t querySize);
--- strutil.c
+++ strutil.c
@@ -1,1 +1,10 @@
 
+#include "./include/strutil.h"
+
+bool wcsStartsWith(wchar_t *tatget,size_t targetSize,wchar_t *query,size_t querySize){
+    //
+}
+
+bool wcsEndsWith(wchar_t *target,size_t targetSize,wchar_t *query,size_t querySize){
+    //
+}
